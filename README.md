09644d55572d27b05a63ede30cb7a0bef22c1200
Benjamin Cable <cable.benjamin@gmail.com>
2015-11-11 18:10:48 +0100

    added readme and documentation
    Signed-off-by: Benjamin Cable <cable.benjamin@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+# 4tools
+4tools provides a convenient way to download all the pictures from a 4chan thread, from any board.
+
+### Installation
+just run `go get github.com/ladydascalie/4tools`
+
+As 4tools doesn't contain any platform specific syscalls, you will be able to it just the same on any platform.
+
+### Usage
+simply call `4tools` from terminal, then paste in the url, press enter and watch it go!
+
+![Usage example](http://i.imgur.com/f68u7N2.png)
--- main.go
+++ main.go
@@ -15,20 +15,21 @@ import (
 )
 
 const (
-	BOARD_STEM string = "//boards.4chan.org"
-	CDN_STEM   string = "//i.4cdn.org"
+	boardStem string = "//boards.4chan.org"
+	cdnStem   string = "//i.4cdn.org"
 )
 
 func main() {
 	fmt.Println("Notice:\nwhen your download is complete,")
 	fmt.Println("you will find your files under: ")
 	fmt.Println(setDownloadFolder() + "\n\n")
-	url := getUrlFromUser()
+	url := getURLFromUser()
 	getImgLinks(url)
 	fmt.Println("\n\nYou can find your downloads under: ", setDownloadFolder())
 }
 
-func getUrlFromUser() string {
+// getURLFromUser grabs the thread URL from the user and returns it
+func getURLFromUser() string {
 	input := bufio.NewReader(os.Stdin)
 	fmt.Println("Thread url ?")
 
@@ -41,6 +42,8 @@ func getUrlFromUser() string {
 	return url
 }
 
+// getImgLinks gets the URL provided by the user then collects all the links containing the CDN Stem in them.
+// It then passes those links to the downloadContent function
 func getImgLinks(url string) {
 	response, _ := http.Get(url)
 
@@ -65,17 +68,17 @@ func getImgLinks(url string) {
 				continue
 			}
 
-			if strings.Contains(href, CDN_STEM) {
-				rawUrl := "http:" + href
-				fmt.Println(rawUrl)
-				downloadContent(rawUrl)
+			if strings.Contains(href, cdnStem) {
+				rawURL := "http:" + href
+				fmt.Println(rawURL)
+				downloadContent(rawURL)
 			}
 		}
+		response.Body.Close()
 	}
-
-	response.Body.Close()
 }
 
+// getHref get the content of the href attribute from <a> tags
 func getHref(t html.Token) (ok bool, href string) {
 	for _, a := range t.Attr {
 		if a.Key == "href" {
@@ -86,6 +89,7 @@ func getHref(t html.Token) (ok bool, href string) {
 	return
 }
 
+// downloadContent makes a get request for the requested file then writes its contents to disk
 func downloadContent(linkTo string) {
 
 	setDownloadFolder()
@@ -115,6 +119,7 @@ func downloadContent(linkTo string) {
 	}
 }
 
+// setDownloadFolder sets the download folder in the user's home folder
 func setDownloadFolder() (dirLocation string) {
 	usr, err := user.Current()
 	if err != nil {
